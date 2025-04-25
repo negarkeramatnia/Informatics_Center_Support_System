57edbdb9c1a25c7c4e153344d90fe10b3b6d04dc
negar keramat nia <negarkrmtnia@gmail.com>
2025-04-25 14:17:52 +0330

    fix issues with controllers and routes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
-class ProfileController extends Controller
+class UserController extends Controller
 {
     public function index()
     {
--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,27 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\AssetController;
+use App\Http\Controllers\MessageReadController;
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-// Route::get('/', function () {
-//     return view('home');
-// });
 
 Route::resource('messages', MessageController::class);
 Route::resource('assets', AssetController::class);
 Route::resource('message-reads', MessageReadController::class);
 Route::resource('tickets', TicketController::class);
-Route::resource('users', ProfileController::class);
+Route::resource('users', UserController::class);
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [UserController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [UserController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [UserController::class, 'destroy'])->name('profile.destroy');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
