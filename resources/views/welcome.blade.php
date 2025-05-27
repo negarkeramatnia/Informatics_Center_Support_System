d828cc00d07b974550a0776a3a3e7f692dfaf5fd
negar keramat nia <negarkrmtnia@gmail.com>
2025-05-27 23:30:22 +0330

    add login and register in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,14 @@
 <body>
     <div style="text-align: center; margin-top: 50px;">
         <h1>Welcome to My Laravel App!</h1>
-        <p>Hello, my name is negar keramat nia :))))))) </p>
-        <p> this is welcome page in my Informatics Center Request and Support System app. </p>
+        <p>Hello, my name is negar keramat nia</p>
+        <p>This is the welcome page in my Informatics Center Request and Support System app.</p>
+        <a href="{{ route('login') }}">
+            <button style="margin: 10px; padding: 10px 20px;">Login</button>
+        </a>
+        <a href="{{ route('register') }}">
+            <button style="margin: 10px; padding: 10px 20px;">Register</button>
+        </a>
     </div>
 </body>
 </html>
