e07bfb049de07c964f4f359b5e0016ff9f819760
negar keramat nia <negarkrmtnia@gmail.com>
2025-05-28 22:41:31 +0330

    add email to users table

--- database/migrations/2025_05_15_104205_create_users_table.php
+++ database/migrations/2025_05_15_104205_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
         $table->id();
         $table->string('name');
         $table->string('username')->unique();
+        $table->string('email')->nullable();
         $table->string('password');
         $table->string('phone')->nullable();
         $table->enum('role', ['admin', 'support', 'user'])->default('user');
