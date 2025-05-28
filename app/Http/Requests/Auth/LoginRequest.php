f6a67606423da6613c5b2ad863e12830993f5dc5
negar keramat nia <negarkrmtnia@gmail.com>
2025-05-28 17:52:09 +0330

    Fix authentication and redirect to dashboard on login

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -27,7 +27,7 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => ['required', 'string', 'email'],
+            'username' => ['required', 'string'],
             'password' => ['required', 'string'],
         ];
     }
@@ -37,19 +37,16 @@ class LoginRequest extends FormRequest
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function authenticate(): void
+    public function authenticate()
     {
-        $this->ensureIsNotRateLimited();
-
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
-            RateLimiter::hit($this->throttleKey());
-
+        if (! Auth::attempt([
+            'username' => $this->input('username'),
+            'password' => $this->input('password'),
+        ], $this->boolean('remember'))) {
             throw ValidationException::withMessages([
-                'email' => trans('auth.failed'),
+                'username' => __('auth.failed'),
             ]);
         }
-
-        RateLimiter::clear($this->throttleKey());
     }
 
     /**
--- database/migrations/2025_05_15_104205_create_users_table.php
+++ database/migrations/2025_05_15_104205_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     Schema::create('users', function (Blueprint $table) {
         $table->id();
         $table->string('name');
-        $table->string('email')->unique();
+        $table->string('username')->unique();
         $table->string('password');
         $table->string('phone')->nullable();
         $table->enum('role', ['admin', 'support', 'user'])->default('user');
